9959b6b7e94ce59540b2b4513006ed6c1a868448
Yoni <yoniakabecky@gmail.com>
2020-02-18 10:24:29 -0800

    Created Product Context

---
+++ src/context/ProductContext.js
@@ -0,0 +1,50 @@
+import React, { Component, createContext } from "react";
+import initialData from "../initialData";
+
+export const ProductContext = createContext();
+
+export default class ProductProvider extends Component {
+  state = {
+    products: [],
+    sortedProducts: [],
+    featuredProducts: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    let products = this.formatData(initialData);
+    let featuredProducts = products.filter(
+      product => product.featured === true
+    );
+    this.setState({
+      products,
+      featuredProducts,
+      sortedProducts: products,
+      loading: false
+    });
+  }
+
+  formatData = products => {
+    let tempProducts = products.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(image => image.fields.file.url);
+      let product = { ...item.fields, images, id };
+      return product;
+    });
+    return tempProducts;
+  };
+
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state
+        }}
+      >
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
+}
+
+export const ProductConsumer = ProductContext.Consumer;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import ProductProvider from "./context/ProductContext";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ProductProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductProvider>,
   document.getElementById("root")
 );
 
