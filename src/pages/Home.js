e0c3891a4c169b2645e05824488e54de8d8fb276
Yoni <yoniakabecky@gmail.com>
2020-02-18 23:34:11 -0800

    Adjusted Hero height

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,13 @@ import Hero from "../components/Hero";
 
 import heroImage from "../images/hero.jpeg";
 import About from "../components/About";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 const Home = () => {
   return (
     <>
-      <Hero img={heroImage} title="macramé shop 37" subtitle="welcome" />
+      <Hero img={heroImage} title="macramé shop 37" subtitle="welcome" full />
+      <FeaturedProducts />
       <About />
     </>
   );
