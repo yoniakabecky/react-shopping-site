99f8631ac449aa6356b880235450023a0aef56f0
Yoni <yoniakabecky@gmail.com>
2020-02-24 11:17:43 -0800

    Modified Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,13 @@
 import React from "react";
 import BodyContainer from "../components/layout/BodyContainer";
+import Hero from "../components/layout/Hero";
+
+import heroImage from "../images/wall.jpg";
 
 const Error = () => {
   return (
     <BodyContainer>
-      <div>Page Not Found</div>
+      <Hero title="page not found" img={heroImage} full />
     </BodyContainer>
   );
 };
